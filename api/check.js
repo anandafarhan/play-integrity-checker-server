7bc539953a21c87e2afa9e857d8b8587d2355309
Ananda Farhan <nandafarhan@gmail.com>
2022-08-16 20:10:03 +0700

    Update credential source

--- api/check.js
+++ api/check.js
@@ -3,15 +3,14 @@ const playintegrity = google.playintegrity('v1');
 
 
 const packageName = process.env.PACKAGE_NAME
-const privatekey = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)
 
 
 async function getTokenResponse(token) {
 
     let jwtClient = new google.auth.JWT(
-        privatekey.client_email,
+        process.env.CLIENT_EMAIL,
         null,
-        privatekey.private_key,
+        process.env.PRIVATE_KEY,
         ['https://www.googleapis.com/auth/playintegrity']);
 
     google.options({ auth: jwtClient });
