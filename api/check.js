229ef1b7dd6f513982f139696c8fd7bcbaf0df8f
Ananda Farhan <nandafarhan@gmail.com>
2022-08-17 14:24:21 +0700

    Update check.js

--- api/check.js
+++ api/check.js
@@ -3,24 +3,15 @@ const playintegrity = google.playintegrity('v1');
 
 const packageName = process.env.PACKAGE_NAME;
 const clientEmail = process.env.CLIENT_EMAIL;
-const keyFile = process.env.GOOGLE_APPLICATION_CREDENTIALS_PATH;
 const privateKey = process.env.PRIVATE_KEY.replace(/\\n/g, '\n');
 const scopes = ['https://www.googleapis.com/auth/playintegrity'];
-// const privatekey = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)
 
 async function getTokenResponse(token) {
+	console.log("======================START OF TOKEN======================");
 	console.log(token);
+	console.log("======================END OF TOKEN======================");
 
 	let client = new google.auth.JWT(clientEmail, undefined, privateKey, scopes);
-
-	// let jwtClient = new google.auth.JWT(privatekey.client_email, null, privatekey.private_key, [
-	// 	'https://www.googleapis.com/auth/playintegrity',
-	// ]);
-
-	// const googleAuth = new google.auth.GoogleAuth({
-	// 	keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS_PATH,
-	// 	scopes: ['https://www.googleapis.com/auth/playintegrity'],
-	// });
 
 	google.options({ auth: client });
 
